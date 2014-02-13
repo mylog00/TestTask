466e0984c28412692e70b3f7de3c8508ac5b9e1e
mylog00 <mylog00@ya.ru>
2014-02-13 13:52:19 +0400

    FirstCommit
    Solve of test task

---
+++ ru/test/task1/Task1Impl.java
@@ -0,0 +1,68 @@
+package ru.tandemservice.test.task1;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * <h1>Задание №1</h1>
+ * Реализуйте интерфейс {@link IStringRowsListSorter}.
+ *
+ * <p>Мы будем обращать внимание в первую очередь на структуру кода и владение стандартными средствами java.</p>
+ */
+public class Task1Impl implements IStringRowsListSorter {
+
+    // ваша реализация должна работать, как singleton. даже при использовании из нескольких потоков.
+    private static final IStringRowsListSorter INSTANCE = new Task1Impl();
+
+	public static final int EQU_VALUE = 0;
+	public static final int NEGATIVE_VALUE = -1;
+	public static final int POSITIVE_VALUE = 1;
+
+	@Override
+    public void sort(final List<String[]> rows, final int columnIndex) {
+
+		Collections.sort(rows, new Comparator<String[]>() {
+			@Override
+			public int compare(final String[] o1, final String[] o2) {
+				//Производим сравнение элементов по индексу
+				final String s1 = o1[columnIndex];
+				final String s2 = o2[columnIndex];
+				// если один из элементов null, то сравниваем строку с null
+				if(s1 == null || s2 == null){
+					return compareToNull(s1, s2);
+				}
+				else {
+					return s1.compareTo(s2);
+				}
+			}
+		});
+		// напишите здесь свою реализацию. Мы ждем от вас хорошо структурированного, документированного и понятного кода.
+    }
+
+	/**
+	 * Сравнивает строки если одна из них null
+	 * @param s1 строка для сравнения
+	 * @param s2 сравниваемая строка
+	 * @return 0 если строки равны, 1 - если сравниваемая строка не null, -1 если сравниваемая строка не null
+	 */
+	private int compareToNull(final String s1, final String s2) {
+		if(s1 == null &&  s2 == null) {
+			return EQU_VALUE;
+		}
+		if(s1 == null){
+			return NEGATIVE_VALUE;
+		}
+		else {
+			return POSITIVE_VALUE;
+		}
+	}
+
+	/**
+	 * Возвращает instance объекта реализующего интерфейс IStringRowsListSorter
+	 * @return объект реализующий интерфейс IStringRowsListSorter
+	 */
+	public static IStringRowsListSorter getInstance(){
+		return INSTANCE;
+	}
+}
---
+++ ru/test/task2/Task2Impl.java
@@ -0,0 +1,123 @@
+package ru.tandemservice.test.task2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
+
+/**
+ * <h1>Задание №2</h1>
+ * Реализуйте интерфейс {@link IElementNumberAssigner}.
+ *
+ * <p>Помимо качества кода, мы будем обращать внимание на оптимальность предложенного алгоритма по времени работы
+ * с учетом скорости выполнения операции присвоения номера:
+ * большим плюсом (хотя это и не обязательно) будет оценка числа операций, доказательство оптимальности
+ * или указание области, в которой алгоритм будет оптимальным.</p>
+ */
+public class Task2Impl implements IElementNumberAssigner {
+
+    // ваша реализация должна работать, как singleton. даже при использовании из нескольких потоков.
+    private static final IElementNumberAssigner INSTANCE = new Task2Impl();
+
+	// глобальная копия списка для присвоения
+	private List<IElement> elements = null;
+	// буферное число bufInt ∉ {number[0], number[1], ... number[n]} (number[i] = elements.get(i).getNumber())
+	private int bufInt = -1;
+
+	/**
+	 * Худшее время выполнения будет если список состоит из пар элементов которые нужно поменять между собой
+	 * тогда количество перестановок будет (N + N/2)
+	 * В среднем количество перестановок будет N для не упорядоченных списков
+	 * и будет снижаться если часть номеров стоят на своих местах
+	 * Учитывая что наиболее длительная операция  присвоение номера {@link IElement#setupNumber(int)}
+	 * то время выполнения будет порядка O(N)
+	 */
+    @Override
+    public void assignNumbers(final List<IElement> elements) {
+
+		// устанавливаем глобальную копию списка для присвоения
+		this.elements = elements;
+
+		// если все элементы были установлены в нужном порядке и работа завершена
+		if (preSort()) return;
+
+		// иначе, проходимся по списку и устанавливаем оставшиеся элементы
+		//for(IElement elem: elements){
+		for(int index = 0; index < elements.size(); index++) {
+			//int index = elements.indexOf(elem);
+			if( index != elements.get(index).getNumber()) {this.bufInt = trySetNumber(index);}
+		}
+
+    }
+
+	/**
+	 * Метод пытается установить все номера из диапазоне от 0 до n не попавшие в список
+	 * @return {@code true} если получилось установить все элементы
+	 */
+	private boolean preSort() {
+		// счетчик количества номеров не принадлежащих диапазону от 0 до n
+		int count = 0;
+
+		// создаем временный список номеров всех элементов
+		List<Integer> listOfAllNumbers = new ArrayList<>();
+		// копируем все номера во временный список для упрощения поиска по ним
+		for(IElement elem: this.elements){
+			listOfAllNumbers.add(elem.getNumber());
+		}
+		// Защищаем список от изменений ()
+		listOfAllNumbers = unmodifiableList(listOfAllNumbers);
+
+		// проверяем все ли номера из диапазона от 0 до n попали в список
+		for (int i = 0; i < listOfAllNumbers.size(); i++){
+			int index = listOfAllNumbers.indexOf(i);
+			// если номер не попал
+			if(index < 0){
+				// пытаемся установить его на свое место и запоминаем замещеное число в буфер
+				this.bufInt = i;
+				this.bufInt = trySetNumber(i);
+				count++;
+			}
+		}
+
+		// если все элементы списка были за пределами диапазона от 0 до n
+		// значит они все были установлены в нужном порядке и работа завершена
+		return ( count >= this.elements.size() );
+	}
+
+	/**
+	 * Пытается заменить число у элемента по индексу {@code number} числом из {@code bufInt}
+	 * @param number индекс элемента {@link IElement} в котором нужно поменять число
+	 * @return замененное значение
+	 */
+	private int trySetNumber(int number){
+		// получаем номер соответствующего элемента списка
+		IElement elem = this.elements.get(number);
+		int elemNumber = elem.getNumber();
+
+		// устанавливаем номером элемента значение буферного числа
+		elem.setupNumber(this.bufInt);
+
+
+		// если заменяемое число не принадлежит диапазону от 0 до n то возвращаем его
+		if( (elemNumber < 0) || (elemNumber >= this.elements.size()) ){
+			return elemNumber;
+		}
+
+		// запоминаем новое значение
+		this.bufInt = elemNumber;
+		// пытаемся установить элемент по индексу elemNumber
+		elemNumber = trySetNumber( elemNumber );
+		return elemNumber;
+	}
+
+
+
+	/**
+	 * Возвращает instance объекта реализующего интерфейс IElementNumberAssigner
+	 * @return объект реализующий интерфейс IElementNumberAssigner
+	 */
+	public static IElementNumberAssigner getInstance(){
+		return INSTANCE;
+	}
+
+}
